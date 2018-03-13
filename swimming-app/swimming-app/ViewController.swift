613b7a6b3dbc582291284a2e6061f35157c857be
TAKAFUMI KAWAI <kawai@keigakusha.jp>
2018-03-13 20:43:20 +0900

    xcodeの初期プロジェクトを作成

---
+++ swimming-app/swimming-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swimming-app
+//
+//  Created by TAKAFUMI KAWAI on 2018/03/13.
+//  Copyright © 2018年 TAKAFUMI KAWAI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
